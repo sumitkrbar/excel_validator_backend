e844bdf5c58cf9e323581b104ecb5e9ea27e4cb8
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2026-02-14 20:04:16 +0530

    Setup project structure and dependencies

---
+++ src/main/java/com/sumit/excelvalidator/ExcelValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.excelvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelValidatorApplication.class, args);
+        System.out.println("Application has started");
+    }
+}
---
+++ src/test/java/com/sumit/excelvalidator/ExcelValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.excelvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
